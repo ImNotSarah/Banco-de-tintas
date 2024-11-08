313abfa368f519e75f7348ef15b5df5218fbc125
ImNotSarah <sarahdejesuslima09@gmail.com>
2024-11-08 11:28:44 -0300

    exibição dos campos CPF, CNPJ, Nome e Nome da Empresa automatizado

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
-console.log('Teste');
+function cpfOuCnpj() {
+    const pessoaFisica = document.querySelector('input[name="tipoPessoa"][value="fisica"]').checked;
+    const campoCPF = document.getElementById("campoCPF");
+    const campoCNPJ = document.getElementById("campoCNPJ");
+    const nomeEmpresa = document.getElementById("nomeEmpresa");
+    const nomePessoa = document.getElementById("nomePessoa");
+
+    if (pessoaFisica) {
+        campoCPF.style.display = "block";
+        nomePessoa.style.display = "block"
+        campoCNPJ.style.display = "none";
+        nomeEmpresa.style.display = "none";
+    } else {
+        campoCPF.style.display = "none";
+        nomePessoa.style.display = "none"
+        campoCNPJ.style.display = "block";
+        nomeEmpresa.style.display = "block";
+    }
+}
